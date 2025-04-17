f5bd971754c4b0a3de7d0e1188d4527172a7cdc3
AhrorovK <akhrorovk5758@gmail.com>
2025-04-18 00:22:54 +0300

    change base url

--- app/src/main/java/krikya/io/soccerapp/core/Constants.kt
+++ app/src/main/java/krikya/io/soccerapp/core/Constants.kt
@@ -1,6 +1,5 @@
 package krikya.io.soccerapp.core
 
 object Constants {
-    const val BASE_URL = "https://v3.football.api-sports.io/"
-    const val apiKey = "bb3963103fccf74eeddbb6a156ca9353"
+    const val BASE_URL = "https://site.api.espn.com/"
 }
--- app/src/main/java/krikya/io/soccerapp/data/network/SoccerApi.kt
+++ app/src/main/java/krikya/io/soccerapp/data/network/SoccerApi.kt
@@ -1,14 +1,11 @@
 package krikya.io.soccerapp.data.network
 
-import krikya.io.soccerapp.core.Constants.apiKey
 import krikya.io.soccerapp.data.model.soccer.SoccerResponse
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface SoccerApi {
-    @GET("sports/soccer/{leagueId}/scoreboard")
+    @GET("apis/site/v2/sports/soccer/{leagueId}/scoreboard")
     suspend fun getScoreboard(
         @Path("leagueId", encoded = true)
         leagueId: String,                  // e.g. "usa.1", "eng.1", "fra.1"
